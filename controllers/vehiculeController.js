1d6625893447dee7925973c9592a4d869c0748af
DanielPilgrim <daniel98trujillo@gmail.com>
2024-08-17 14:44:55 -0500

    delete and put

--- controllers/vehiculeController.js
+++ controllers/vehiculeController.js
@@ -1,10 +1,11 @@
+const vehicule = require('../models/vehicule')
 const Vehicule = require('../models/vehicule')
-
+///////metodo get
 const getVehicule = async(req,res) => {
     const vehicules = await Vehicule.find()
     res.json(vehicules)
 }
-
+//////metodo post
 const postVehicule = async(req,res) => {
     let msg = 'vehicule inserted'
     const body =req.body
@@ -16,21 +17,31 @@ const postVehicule = async(req,res) => {
     }
     res.json({msg:msg})
 }
-
+/////metodo put
 const putVehicule = async(req, res) => {
-    let msg = 'vehicule inserted'
-    const body =req.body
+    let msg = 'vehicule updated'
+    const {plate,color,model}=req.body
     try{
-        const vehicule = new Vehicule(body)
-        await vehicule.save()
+        await vehicule.findOneAndUpdate({plate:plate,color:color,model:model})
     }catch(error){
         msg = error
     }
     res.json({msg:msg})
 }
-
+/////metodo delete
+const deleteVehicule = async (req,res) => {
+    let msg = 'vehicule deleted'
+    id = req.params.id
+    try{
+        await vehicule.findOneAndDelete({_id:id})
+    }catch(error){
+        msg = 'there was an error deleting'
+    }
+    res.json({msg:msg})
+}
 module.exports = {
     getVehicule,
     postVehicule,
-    putVehicule
+    putVehicule,
+    deleteVehicule
 }
